d68d5a75c64ed6b4234821224cd0f0444e977802
森部高昌 <takamasamoribe@gmail.com>
2021-10-09 17:03:31 +0900

    1009 1 Current Location 現在地を表示するようにしてみる

--- TopoMap/ViewController.swift
+++ TopoMap/ViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,,CLLocationManagerDelegate,MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
 
@@ -23,17 +24,80 @@ class ViewController: UIViewController {
     private let tileOverlay = MKTileOverlay(urlTemplate: "https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png")
     //https://cyberjapandata.gsi.go.jp/xyz/relief/{z}/{x}/{y}.png
 
+    // ロケーションマネージャーのインスタンス
+    var locManager: CLLocationManager!
+    
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        mapView.delegate = self
+//        mapView.addOverlay(tileOverlay, level: .aboveLabels)
+//    }
+
+    //======================================================
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locManager = CLLocationManager()
+        locManager.delegate = self
+ 
+        locManager.desiredAccuracy = kCLLocationAccuracyHundredMeters //誤差100m程度の精度
+        //kCLLocationAccuracyNearestTenMeters    誤差10m程度の精度
+        //kCLLocationAccuracyBest    最高精度(デフォルト値)
+        locManager.distanceFilter = 5//精度は5ｍにしてみた
 
+        // 位置情報の使用の許可を得て、取得する
+        locManager.requestWhenInUseAuthorization()
+
+        locationManagerDidChangeAuthorization(locManager)
+            //authorizationStatus() がdeprecated になったため、上のメソッドで対応している
+        
+                mapView.delegate = self
+                mapView.addOverlay(tileOverlay, level: .aboveLabels)
+        
+        }
+    
+    //======================================================
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+     let status = manager.authorizationStatus
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            locManager.startUpdatingLocation()// 取得を開始する
+            break
+        case .notDetermined, .denied, .restricted:
+            break
+        default:
+            break
+        }
+    }
+    
+    // CLLocationManagerのdelegate：現在位置取得
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]) {
+        
+        //現在地の緯度経度取得 ido,keido
+        let location:CLLocation = locations[0]//locations[0]の意味
+        let ido = location.coordinate.latitude
+        let keido = location.coordinate.longitude
+
+        //"mapView"に地図を表示する　よくある範囲設定をしてみた
+        var region:MKCoordinateRegion = mapView.region
+        region.span.latitudeDelta = 0.01
+        region.span.longitudeDelta = 0.01
+        
+        let compass = MKCompassButton(mapView: mapView) // コンパス
+        compass.frame = CGRect(x:300,y:15,width:5,height:5) // 位置と大きさ
+        self.view.addSubview(compass)// コンパスを地図に表示する
+        mapView.userTrackingMode = .followWithHeading // 現在地付近の地図
         mapView.delegate = self
-        mapView.addOverlay(tileOverlay, level: .aboveLabels)
+        
     }
 }
 
-extension ViewController: MKMapViewDelegate {
+
+//extension ViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         return MKTileOverlayRenderer(overlay: overlay)
     }
-}
+//}
 
