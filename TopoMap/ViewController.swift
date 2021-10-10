c988a69c4588faab1268ae4c57d5b041ce2e6069
森部高昌 <takamasamoribe@gmail.com>
2021-10-10 11:57:31 +0900

    1010  5  地名の緯度経度検索ができた

--- TopoMap/ViewController.swift
+++ TopoMap/ViewController.swift
@@ -57,7 +57,6 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
         }
     
     
-    
 //--------------------------------------------------
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
       //キーボードを閉じる。resignFirstResponderはdelegateメソッド
@@ -66,12 +65,28 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
         if let searchKey = textField.text {
          //入力された文字をデバッグエリアの表示
          print(searchKey)
-       }
+        //CLGeocoderインスタンスを取得
+        let geocoder = CLGeocoder()
+        //入力された文字から位置情報を取得
+        geocoder.geocodeAddressString(searchKey, completionHandler: { (placemarks, error) in
+        //位置情報が存在する場合（定数geocoderに値が入ってる場合)はunwrapPlacemarksに取り出す。
+            if let unwrapPlacemarks = placemarks {
+              //1件目の情報を取り出す
+             if let firstPlacemark = unwrapPlacemarks.first {
+               //位置情報を取り出す
+               if let location = firstPlacemark.location {
+                 //位置情報から緯度経度をtargetCoordinateに取り出す
+                  let targetCoordinate = location.coordinate
+                  //緯度経度をデバッグエリアに表示
+                  print(targetCoordinate)
+               }
+              }
+            }
+            })
+        }
         //デフォルト動作を行うのでtureを返す。返り値型をBoolにしているため、この記述がないとエラーになる。
        return true
     }
-    
-    
     
     
     //======================================================
