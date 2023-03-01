68abc8c7dfb3cc3bcf9b225226f2e8ad380c542e
森部高昌 <takamasamoribe@gmail.com>
2023-03-01 21:09:01 +0900

    0301-03  線は引くが、検索地点が画面中央になっている。

--- TopoMap/ViewController.swift
+++ TopoMap/ViewController.swift
@@ -38,7 +38,10 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
     // ロケーションマネージャーのインスタンスを生成する
     var locManager: CLLocationManager!
     
-            // 検索地点の初期値を設定しておく。表示はされない。
+    // 現在地の初期値を設定しておく。表示はされない。
+            var myLatitude:Double = 35.67485 // 自宅の緯度35.67485 139.80615
+            var myLongitude:Double = 139.80615 // 自宅の経度139.80615
+    // 検索地点の初期値を設定しておく。表示はされない。
             var selectedPlace:String = "木場公園"
             var selectedAddress:String = "〒135-0042,東京都江東区,木場４丁目"
             var targetLatitude:Double = 35.6743169 // 木場公園の緯度
@@ -83,24 +86,40 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
         self.present(nextView, animated: true, completion: nil)
     }
 
-    // ツールバー内の「現在地」ボタンをクリックした時
+    // ツールバー内の「現在地」ボタンをクリックした時、現在地の緯度経度を取得する
     @IBAction func currentButtonClicked(_ sender: UIBarButtonItem) {
         // 現在地の取得 ロケーションマネージャーのインスタンスを作成する
         locManager = CLLocationManager()
-        locManager.delegate = self // 現在地を取得して表示する？
+        locManager.delegate = self // 現在地を取得して表示する
         //locManager.requestLocation()
         //locManager.desiredAccuracy = kCLLocationAccuracyHundredMeters//誤差100m程度の精度
         //                          kCLLocationAccuracyNearestTenMeters//誤差10m程度の精度
         //                          kCLLocationAccuracyBest//最高精度(デフォルト値)
         //locManager.distanceFilter = 10//10ｍ移動したら、位置情報を更新する
+        
     }
     
-    // ツールバー内の 矢印アイコン　をクリックした時　現在地から目的地へ線を引く
+    // ツールバー内の 矢印アイコン　をクリックした時　現在地を取得してから目的地へ線を引く
     @IBAction func drawButtonClicked(_ sender: UIBarButtonItem) {
         
-        // ここに線を引くメソッドへのリンクを置く
-        //locManager = CLLocationManager()
-        //locManager.delegate = self // 現在地取得へ
+        // 現在地の取得
+        locManager = CLLocationManager()
+        locManager.delegate = self // 現在地取得へ
+        
+        // ここから線を引く準備
+        // 現在地の座標・・・直前に CLLocationManager() を使って取得した
+        let locNow = CLLocationCoordinate2D(latitude: myLatitude, longitude: myLongitude)
+        print("取得した現在地の緯度は、\(myLatitude)")
+        print("取得した現在地の経度は、\(myLongitude)")
+        
+        // 検索地点の座標・・・これは、前回検索地点を読み込む
+        let locTarget = CLLocationCoordinate2D(latitude: targetLatitude, longitude: targetLongitude)
+        print("検索地点 selectedPlace:\(selectedPlace)")
+        print("検索地点の緯度\(targetLatitude)")
+        print("検索地点の経度\(targetLongitude)")
+        
+        // 線を引くメソッドへ
+        drawLine(current: locNow, destination: locTarget) //
         
     }
     
@@ -108,8 +127,6 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
     //==============================================================================
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //mapView.delegate = self //Mapの描画
 
         // 目標地点として、前回の検索で保存しておいた値を読み込む
         selectedPlace = UserDefaults.standard.string(forKey: "targetPlace")!
@@ -161,43 +178,37 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
         //   　mapView.userTrackingMode = .none // 現在地の更新をしない
         //   //mapView.userTrackingMode = .follow // 現在地の更新をする
         // }
-        
+ 
+        // --------------------------------------------------------------
         //現在地の緯度経度を取得する myLatitude,myLongitude
-        let location:CLLocation = locations[0]//locations[0]の意味
-        let myLatitude = location.coordinate.latitude //現在地の緯度
-        let myLongitude = location.coordinate.longitude //現在地の経度
-        // 現在地の座標
-        let locNow = CLLocationCoordinate2D(latitude: myLatitude, longitude: myLongitude)
-        print("現在地の緯度\(myLatitude)")
-        print("現在地の経度\(myLongitude)")
+        //let location:CLLocation = locations[0]//locations[0]の意味
+        //let myLatitude = location.coordinate.latitude //現在地の緯度
+        //let myLongitude = location.coordinate.longitude //現在地の経度
         
-        // ここから線を引く準備。
-        // 検索地点の座標
-        let locTarget = CLLocationCoordinate2D(latitude: targetLatitude, longitude: targetLongitude)
-        print("検索地点 selectedPlace:\(selectedPlace)")
-        print("検索地点の緯度\(targetLatitude)")
-        print("検索地点の経度\(targetLongitude)")
-        
-        // メソッドで線を引く
-        drawLine(current: locNow, destination: locTarget) // 
-
-//        // 現在地と検索地点、２点の座標を入れた配列をつくる
-//        let lineArray = [locNow,locTarget]
+//    // --------------------------------------------------------------
+//        // 現在地の座標
+//        let locNow = CLLocationCoordinate2D(latitude: myLatitude, longitude: myLongitude)
+//        print("現在地の緯度\(myLatitude)")
+//        print("現在地の経度\(myLongitude)")
 //
-//        print("線を引きます")
-//        // ２点を結ぶ線を引く。（緯度,経度）＝（０、０）　未設定の時は線を引かない
-//        mapView.delegate = self //Mapの描画
-//            if (targetLatitude != 0) && (targetLongitude != 0) {
-//                let redLine = MKPolyline(coordinates: lineArray, count: 2)
-//                mapView.addOverlays([redLine])// 地図上に描く
-//            }
+//        // ここから線を引く準備。
+//        // 検索地点の座標・・・これは、前回検索地点を読み込む
+//        let locTarget = CLLocationCoordinate2D(latitude: targetLatitude, longitude: targetLongitude)
+//        print("検索地点 selectedPlace:\(selectedPlace)")
+//        print("検索地点の緯度\(targetLatitude)")
+//        print("検索地点の経度\(targetLongitude)")
+//
+//        // 線を引くメソッドへ
+//        drawLine(current: locNow, destination: locTarget) //
+//    // --------------------------------------------------------------
+
     }
     
     // 線を引くメソッド
     func drawLine(current:CLLocationCoordinate2D,destination:CLLocationCoordinate2D)  {
-        // 現在地と検索地点、２点の座標を入れた配列をつくる
+        // 現在地と目的地、２点の座標を入れた配列をつくる
         let lineArray = [current,destination]
-        print("メソッドで線を引きます")
+        print("線を引くメソッドの中です")
         // ２点を結ぶ線を引く。（緯度,経度）＝（０、０）　未設定の時は線を引かない
         mapView.delegate = self //Mapの描画
             if (targetLatitude != 0) && (targetLongitude != 0) {
